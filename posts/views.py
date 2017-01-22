7b00ee44bb4954ae7bc189344e5c0cdc069f8819
shivammutreja <shivammutreja25@gmail.com>
2017-01-22 23:23:00 +0530

    Model form and create view

--- posts/views.py
+++ posts/views.py
@@ -3,26 +3,38 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-    return HttpResponse("<h3>Hello!</h3>")
+        form = PostForm(request.POST or None)
+        
+        context = {
+                "form": form,
+                }
+        
+        if form.is_valid():
+                form.save(commit=False)
+                form.save()
+                
+        return render(request, "post_form.html", context)
+
 
 def post_detail(request, id=None):
-    instance = get_object_or_404(Post, id=id)
-    context = {
+	instance = get_object_or_404(Post, id=id)
+	context = {
             "instance": instance,
             "title": instance.title
             }
-    return render(request, "detail.html", context)
+	return render(request, "detail.html", context)
 
 def post_list(request):
-    query_set = Post.objects.all()
-    context = {
+	query_set = Post.objects.all()
+	context = {
             "object_list": query_set,
             "title": "List"
             }
-    return render(request, "index.html", context)
+	return render(request, "index.html", context)
 
 def post_update(request):
     return HttpResponse("<h3>Hello!</h3>")
