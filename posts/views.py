3a87e3f0ceca794d76ac6496acb53bc383fbbf24
shivammutreja <shivammutreja25@gmail.com>
2017-01-24 00:06:15 +0530

    instance update view

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
@@ -8,15 +8,16 @@ from .models import Post
 
 def post_create(request):
         form = PostForm(request.POST or None)
-        
+       
+        if form.is_valid():
+                instance = form.save(commit=False)
+                instance.save()
+                return HttpResponseRedirect(instance.get_absolute_url())
+
         context = {
                 "form": form,
                 }
         
-        if form.is_valid():
-                form.save(commit=False)
-                form.save()
-                
         return render(request, "post_form.html", context)
 
 
@@ -36,8 +37,23 @@ def post_list(request):
             }
 	return render(request, "index.html", context)
 
-def post_update(request):
-    return HttpResponse("<h3>Hello!</h3>")
+def post_update(request, id=None):
+        instance = get_object_or_404(Post, id=id)
+
+        form = PostForm(request.POST or None, instance=instance)
+        
+        if form.is_valid():
+                instance = form.save(commit=False)
+                instance.save()
+                return HttpResponseRedirect(instance.get_absolute_url())
+
+        context = {
+            "instance": instance,
+            "title": instance.title,
+            "form": form,
+            }
+
+        return render(request, "post_form.html", context)
 
 def post_delete(request):
     return HttpResponse("<h3>Hello!</h3>")
